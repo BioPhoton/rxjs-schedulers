bbc8435161b70fcc1c18e9b5d67552358f8e871e
Michael Hladky <michael@hladky.at>
2018-05-09 03:41:14 +0200

    fix(webcomponents): fix removed polyfill for webcomponents from main app

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -77,4 +77,3 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-import '@webcomponents/custom-elements/custom-elements.min';
