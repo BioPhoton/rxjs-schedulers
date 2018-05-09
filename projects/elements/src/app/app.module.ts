4c0d9df62d4451c35a1275e808818b9b66633789
Michael Hladky <michael@hladky.at>
2018-05-09 03:30:29 +0200

    fix(webcomponents): refactor creation

--- projects/elements/src/app/app.module.ts
+++ projects/elements/src/app/app.module.ts
@@ -1,20 +1,36 @@
 import {Injector, NgModule} from '@angular/core';
-import {createCustomElement} from '@angular/elements';
+import {createCustomElement, NgElementConfig} from '@angular/elements';
 import {BrowserModule} from '@angular/platform-browser';
 import {SchedulersAndAnimationsComponent} from './schedulers-and-animations/schedulers-and-animations.component';
 import {SchedulersAndAnimationsModule} from './schedulers-and-animations/schedulers-and-animations.module';
+import {BindingsComponentComponent} from './simple-components/bindings-component.component';
+import {SimpleComponentComponent} from './simple-components/simple-component.component';
+import {SimpleComponentModule} from './simple-components/simple-component.module';
 
 @NgModule({
-  imports: [BrowserModule, SchedulersAndAnimationsModule],
+  imports: [
+    BrowserModule,
+    SchedulersAndAnimationsModule,
+    SimpleComponentModule],
   declarations: []
 })
 export class AppModule {
+
   constructor(private injector: Injector) {
   }
 
   ngDoBootstrap() {
-    const ngElement = createCustomElement(SchedulersAndAnimationsComponent, {injector: this.injector});
-    customElements.define('schedulers-and-animations', ngElement);
-  }
-}
+    const elements = [
+      SchedulersAndAnimationsComponent,
+      SimpleComponentComponent,
+      BindingsComponentComponent
+    ];
 
+    const config: NgElementConfig = {injector: this.injector};
+    elements.forEach((cl) => {
+      const ngElement = createCustomElement(cl, paramsObj);
+      customElements.define(cl.selector, ngElement);
+    });
+  }
+
+}
